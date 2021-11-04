427bf75f6cf824537584fcc615af4518bf277b11
Alejandro Arroyo <alejandro.arroyo@ons.gov.uk>
2021-11-04 09:43:03 +0000

    app now redirects to frontstage on dev (localhsot)

--- mock_eq/routes.py
+++ mock_eq/routes.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from mock_eq import app
 
 @app.route("/")
 @app.route("/mock-eq", methods=['GET'])
 def mock_eq():
+    # set eQ to INPROGRESS
     return render_template('base.html', title='Base Page')
+
+@app.route("/receipt")
+def receipt():
+    # set up pubsub receipt
+    # set eQ to COMPLETE
+    return redirect(location = "http://localhost:8082")
