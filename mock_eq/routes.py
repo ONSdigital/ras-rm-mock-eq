101eb31f196273e7c93f57d8b20156faeb572892
Alejandro Arroyo <alejandro.arroyo@ons.gov.uk>
2021-11-15 09:19:29 +0000

    added decrypter

--- mock_eq/routes.py
+++ mock_eq/routes.py
@@ -1,14 +1,46 @@
 from flask import Flask, render_template, redirect, request
 from mock_eq import app
 
+from sdc.crypto.decrypter import decrypt
+from sdc.crypto.key_store import KeyStore, validate_required_keys
+
+import os
+import json
+
+KEY_PURPOSE = "authentication"
+
 @app.route("/")
 @app.route("/session", methods=['GET'])
 def mock_eq():
-    token = request.args.get('token', None)
+    payload = request.args.get('token', None)
+
+    json_secret_keys = app.config["JSON_SECRET_KEYS"]
+    print(json_secret_keys)
+    decrypter = Decrypter(json_secret_keys)
+
+    # decrypted_json = decrypter.decrypt(payload)
+    # print(decrypted_json)
     return render_template('base.html', title='Mock eQ')
 
 @app.route("/receipt")
 def receipt():
-    # set up pubsub receipt
-    # set eQ to COMPLETE
-    return redirect("http://localhost:8082/surveys/todo")
+    return redirect(app.config["FRONTSTAGE_URL"])
+
+
+
+
+
+class Decrypter:
+    def __init__(self, json_secret_keys):
+        keys = json.loads(json_secret_keys)
+        validate_required_keys(keys, KEY_PURPOSE)
+        self.key_store = KeyStore(keys)
+
+    def decrypt(self, payload):
+        """
+        Decrypts the payload using the keystore values
+        :param payload: the value to decrypt
+        :return: string of decrypted payload
+        """
+        decrypted_json = decrypt(payload, key_store=self.key_store, key_purpose=KEY_PURPOSE)
+        return decrypted_json
