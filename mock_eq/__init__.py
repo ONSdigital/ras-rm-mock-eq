7f88a4c7109902b60923236eebc39b43fa95f05e
Alejandro Arroyo <alejandro.arroyo@ons.gov.uk>
2021-11-16 12:17:31 +0000

    changed config to dev by default

--- mock_eq/__init__.py
+++ mock_eq/__init__.py
@@ -4,7 +4,7 @@ import os
 app = Flask(__name__)
 
 # Load app config
-app_config = "config.{}".format(os.environ.get("APP_SETTINGS", "Config"))
+app_config = "config.{}".format(os.environ.get("APP_SETTINGS", "DevelopmentConfig"))
 app.config.from_object(app_config)
 
 from mock_eq import routes
