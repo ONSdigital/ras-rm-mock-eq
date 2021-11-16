fbbc564b976e033d8715dc4e79147e11a60bfb1d
Alejandro Arroyo <alejandro.arroyo@ons.gov.uk>
2021-11-16 15:58:31 +0000

    pull policy set to always

--- config.py
+++ config.py
@@ -6,4 +6,3 @@ class DevelopmentConfig(object):
     FRONTSTAGE_URL = os.getenv("FRONTSTAGE_URL", "http://localhost:8082/surveys/todo")
     # JSON_SECRET_KEYS = os.getenv("JSON_SECRET_KEYS") or open("./tests/test_data/jwt-test-keys/test_key.json").read()
     JSON_SECRET_KEYS = os.getenv("JSON_SECRET_KEYS")
-    
