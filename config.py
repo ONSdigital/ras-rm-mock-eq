1d28f1d74f292c607f7e3995e4fe4a71b1e1633a
Alejandro Arroyo <alejandro.arroyo@ons.gov.uk>
2021-11-16 13:20:16 +0000

    removed config in config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 import os
 
 
-class Config(object):
-    PORT = os.getenv("PORT", 8086)
-
-
-class DevelopmentConfig(Config):
+class DevelopmentConfig(object):
     PORT = os.getenv("PORT", 8086)
     FRONTSTAGE_URL = os.getenv("FRONTSTAGE_URL", "http://localhost:8082/surveys/todo")
     JSON_SECRET_KEYS = os.getenv("JSON_SECRET_KEYS") or open("./tests/test_data/jwt-test-keys/test_key.json").read()
