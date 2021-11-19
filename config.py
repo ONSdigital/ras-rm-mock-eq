fe820c107a507a9653bf15f73b93b7cdc2a10376
Alejandro Arroyo <alejandro.arroyo@ons.gov.uk>
2021-11-19 15:21:04 +0000

    added version to config

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 
 class DevelopmentConfig(object):
+    VERSION = "1.0.9"
     PORT = os.getenv("PORT", 8086)
     FRONTSTAGE_URL = os.getenv("FRONTSTAGE_URL", "http://localhost:8082/surveys/todo")
     JSON_SECRET_KEYS = os.getenv("JSON_SECRET_KEYS") or open("./tests/test_data/jwt-test-keys/test_key.json").read()
