2d5167ef04720b28172a78a13135681206246e8b
LJBabbage <ljbabbage@hotmail.com>
2024-01-16 10:44:18 +0000

    RAS-918 Write out JWT in mock-eq v3 template

--- mock_eq/routes.py
+++ mock_eq/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, request, redirect, flash, jsonify
+from flask import render_template, request, redirect, flash, jsonify, url_for
 from mock_eq import app
 
 from mock_eq.common.decrypter import Decrypter
 from mock_eq.common.pubsub import PubSub
+from sdc.crypto.exceptions import InvalidTokenException, CryptoError
 
 import logging
 import structlog
@@ -22,40 +23,46 @@ def mock_eq():
 
 @app.route("/v3/session", methods=["GET"])
 def mock_eq_v3():
-    # A similar endpoint to represent the EQ v3 site that now exists.  When it comes to the decryption in the /receipt
-    # endpoint, the token should have the kid of key it needs to use.  If the keys have been set up correctly, it should
-    # work for both eq and eq v3 without us having to do anything.
-    payload = request.args.get("token", None)
-    if payload is None:
-        logger.error("No payload passed from frontstage")
-        flash("No payload passed from frontstage")
+    if not (token:= request.args.get("token")):
+        logger.error("No token passed")
+        return render_template("errors/400-error.html", frontstage=app.config["FRONTSTAGE_URL"])
+
+    try:
+        decrypter = Decrypter(app.config["JSON_SECRET_KEYS"])
+        payload = decrypter.decrypt(token)
+    except (InvalidTokenException, CryptoError) as e:
+        logger.error("An error happened when decrypting the token", error=e.value)
+        return render_template("errors/400-error.html", frontstage=app.config["FRONTSTAGE_URL"])
+
+    data = payload["survey_metadata"]["data"]
+
+    receipt_url = url_for(
+        "receipt",
+        case_ref=data["case_ref"],
+        case_id=payload["case_id"],
+        party_id=data["user_id"],
+        sds_dataset_id=data.get("sds_dataset_id"),
+    )
+
     return render_template(
-        "mock_eq_v3.html", title="Mock eQ v3", frontstage=app.config["FRONTSTAGE_URL"], payload=payload
+        "mock_eq_v3.html",
+        title="Mock eQ v3",
+        frontstage=app.config["FRONTSTAGE_URL"],
+        receipt_url=receipt_url,
+        payload=payload,
     )
 
 
 @app.route("/receipt", methods=["GET"])
 def receipt():
-    payload = request.args.get("token", None)
-
-    try:
-        json_secret_keys = app.config["JSON_SECRET_KEYS"]
-        decrypter = Decrypter(json_secret_keys)
-
-        json_payload = decrypter.decrypt(payload)
-        pubsub_payload = {
-            "caseRef": json_payload["survey_metadata"]["data"]["case_ref"],
-            "caseId": json_payload["case_id"],
-            "inboundChannel": "OFFLINE",
-            "partyId": json_payload["survey_metadata"]["data"]["user_id"]
-        }
-
-        if "sds_dataset_id" in json_payload["survey_metadata"]["data"]:
-            pubsub_payload["sdsDatasetId"] = json_payload["survey_metadata"]["data"]["sds_dataset_id"]
-
-    except Exception:
-        logger.error("An error happened when decrypting the frontstage payload", exc_info=True)
-        return render_template("errors/500-error.html", frontstage=app.config["FRONTSTAGE_URL"])
+    pubsub_payload = {
+        "caseRef": request.args.get("case_ref"),
+        "caseId": request.args.get("case_id"),
+        "inboundChannel": "OFFLINE",
+        "partyId": request.args.get("party_id"),
+    }
+    if request.args.get("sds_dataset_id"):
+        pubsub_payload["sdsDatasetId"] = request.args.get("sds_dataset_id")
 
     publisher = PubSub(app.config)
     publisher.publish(pubsub_payload)
